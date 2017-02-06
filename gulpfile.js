3f0b7c88c7a0c9818123f0958059c303e7aaeceb
Damian McNulty <damianmcnulty@me.com>
2017-02-06 16:34:40 +0000

    host site on github

--- gulpfile.js
+++ gulpfile.js
@@ -40,7 +40,7 @@ gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
 gulp.task('browser-sync', ['sass', 'jekyll-build'], function() {
     browserSync({
         server: {
-            baseDir: '_site'
+            baseDir: 'site'
         },
         notify: false
     });
